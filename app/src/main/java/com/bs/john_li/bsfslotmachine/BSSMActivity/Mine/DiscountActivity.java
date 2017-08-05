f713852bf69fbee2af3ed244c76c322d5dfc120a
17710622616 <m17710622616@163.com>
2017-08-06 00:18:26 +0800

    整改界面，主要是下方的三個按鈕界面風格及圖片                  其他也不知道改了什麼，就這麼備註吧                  2017.8.3:做了狀態欄變色的操作，投機取巧的方法，直接頂上去，然後置頂， 然後設置padding，還有我的 2017.8.4写了我的界面里设置，个人信息，个人资产三个按钮及里面的界面

--- app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMActivity/Mine/DiscountActivity.java
+++ app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMActivity/Mine/DiscountActivity.java
@@ -3,10 +3,15 @@ package com.bs.john_li.bsfslotmachine.BSSMActivity.Mine;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.ListView;
 
 import com.bs.john_li.bsfslotmachine.BSSMActivity.BaseActivity;
+import com.bs.john_li.bsfslotmachine.BSSMAdapter.DiscountAdapter;
 import com.bs.john_li.bsfslotmachine.BSSMView.BSSMHeadView;
 import com.bs.john_li.bsfslotmachine.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 優惠券
@@ -15,6 +20,8 @@ import com.bs.john_li.bsfslotmachine.R;
 
 public class DiscountActivity extends BaseActivity implements View.OnClickListener{
     private BSSMHeadView discount_head;
+    private ListView discountLv;
+    private List<String> discountList;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,7 @@ public class DiscountActivity extends BaseActivity implements View.OnClickListen
     @Override
     public void initView() {
         discount_head = findViewById(R.id.discount_head);
+        discountLv = findViewById(R.id.discount_lv);
     }
 
     @Override
@@ -38,6 +46,12 @@ public class DiscountActivity extends BaseActivity implements View.OnClickListen
     public void initData() {
         discount_head.setTitle("優惠券");
         discount_head.setLeft(this);
+
+        discountList = new ArrayList<>();
+        for (int i = 0;i<3;i++) {
+            discountList.add("优惠券"+i);
+        }
+        discountLv.setAdapter(new DiscountAdapter(this, discountList));
     }
 
     @Override
---
+++ app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMAdapter/DiscountAdapter.java
@@ -0,0 +1,67 @@
+package com.bs.john_li.bsfslotmachine.BSSMAdapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.bs.john_li.bsfslotmachine.R;
+
+import java.util.List;
+
+/**
+ * Created by John_Li on 6/8/2017.
+ */
+
+public class DiscountAdapter extends BaseAdapter {
+    private List<String> discountList;
+    private LayoutInflater inflater;
+    public  DiscountAdapter(Context context,List<String> discountList){
+        this.discountList = discountList;
+        inflater = LayoutInflater.from(context);
+    }
+    @Override
+    public int getCount() {
+        return discountList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return discountList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View contentView = view;
+        ViewHolder holder = null;
+        if (contentView == null){
+            holder = new ViewHolder();
+            contentView = inflater.inflate(R.layout.item_discount, null);
+            holder.moneyTv = contentView.findViewById(R.id.item_discount_money);
+            contentView.setTag(holder);
+        } else {
+            holder = (ViewHolder) contentView.getTag();
+        }
+        holder.moneyTv.setText(discountList.get(i));
+        return contentView;
+    }
+
+    public class ViewHolder{
+        public TextView moneyTv;
+
+        public TextView getMoneyTv() {
+            return moneyTv;
+        }
+
+        public void setMoneyTv(TextView moneyTv) {
+            this.moneyTv = moneyTv;
+        }
+    }
+}
