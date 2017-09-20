8c8ddbfc43243452e5857c575b4b05c537ef83c3
John <m17710622616@163.com>
2017-09-21 00:18:46 +0800

    寫了登錄界面，但是call接口415

--- app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMActivity/Parking/SearchSlotMachineActivity.java
+++ app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMActivity/Parking/SearchSlotMachineActivity.java
@@ -2,27 +2,43 @@ package com.bs.john_li.bsfslotmachine.BSSMActivity.Parking;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.SearchView;
 
 import com.bs.john_li.bsfslotmachine.BSSMActivity.BaseActivity;
+import com.bs.john_li.bsfslotmachine.R;
 
 /**
+ * 搜索界面
  * Created by John_Li on 21/9/2017.
  */
 
 public class SearchSlotMachineActivity extends BaseActivity {
+    private SearchView mSearchView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_slot_machine);
     }
 
     @Override
     public void initView() {
-
+        mSearchView = findViewById(R.id.search_slot_sv);
+        mSearchView.onActionViewExpanded();
     }
 
     @Override
     public void setListener() {
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
     @Override
