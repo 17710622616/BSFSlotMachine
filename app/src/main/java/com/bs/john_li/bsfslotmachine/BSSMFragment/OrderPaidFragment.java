3e2807d9a6431df7fc7a61d3ea4a52ddfe2e1ad7
17710622616 <m17710622616@163.com>
2018-01-06 07:18:24 +0800

    根據後台修改了停車的一些數據

--- app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMFragment/OrderPaidFragment.java
+++ app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMFragment/OrderPaidFragment.java
@@ -3,23 +3,133 @@ package com.bs.john_li.bsfslotmachine.BSSMFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bs.john_li.bsfslotmachine.BSSMAdapter.SmartOrderRefreshAdapter;
+import com.bs.john_li.bsfslotmachine.BSSMModel.UserOrderOutModel;
 import com.bs.john_li.bsfslotmachine.R;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.listener.OnLoadmoreListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 已支付訂單
  * Created by John_Li on 5/1/2018.
  */
 
-public class OrderPaidFragment extends Fragment {
+public class OrderPaidFragment extends LazyLoadBaseFragment {
+    private View view;
+    private RefreshLayout mRefreshLayout;
+    private RecyclerView mRecycleView;
+    private List<UserOrderOutModel.UserOrderInsideModel.UserOrderModel> orderList;
+    private SmartOrderRefreshAdapter mSmartOrderRefreshAdapter;
+    private LinearLayout noOrderLL;
+    // 每頁加載數量
+    private int pageSize = 10;
+    // 頁數
+    private int pageNo = 1;
+    // 車輛總數
+    private long totolCarCount;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_orderlist, null);
+        if(view == null){
+            view = inflater.inflate(R.layout.fragment_orderlist, null);
+        }
+
+        initView();
+        setListener();
+        initData();
         return view;
+    }
+
+    private void initView() {
+        mRefreshLayout = view.findViewById(R.id.order_list_srl);
+        mRecycleView = view.findViewById(R.id.order_list_lv);
+        noOrderLL = view.findViewById(R.id.no_order_ll);
+    }
+
+    private void setListener() {
+        mRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                orderList.clear();
+                pageNo = 1;
+                callNetGetCarList();
+            }
+        });
+        mRefreshLayout.setOnLoadmoreListener(new OnLoadmoreListener() {
+            @Override
+            public void onLoadmore(RefreshLayout refreshlayout) {
+                //和最大的数据比较
+                if (pageSize * (pageNo + 1) > totolCarCount){
+                    Toast.makeText(getActivity(), "沒有更多數據了誒~", Toast.LENGTH_SHORT).show();
+                    mRefreshLayout.finishRefresh();
+                    mRefreshLayout.finishLoadmore();
+                } else {
+                    pageNo ++;
+                    callNetGetCarList();
+                }
+            }
+        });
+    }
+
+    private void initData() {
+        orderList = new ArrayList<>();
+        mSmartOrderRefreshAdapter = new SmartOrderRefreshAdapter(getActivity(), orderList);
+        mRecycleView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecycleView.setAdapter(mSmartOrderRefreshAdapter);
+
+        mSmartOrderRefreshAdapter.setOnItemClickListenr(new SmartOrderRefreshAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                /*Intent intent = new Intent(getActivity(), ArticleDetialActivity.class);
+                intent.putExtra("startway", 0);
+                intent.putExtra("ContentsModel", new Gson().toJson(contentsList.get(position)));
+                startActivity(intent);*/
+            }
+        });
+    }
+
+    @Override
+    protected void onFragmentVisibleChange(boolean isVisible) {
+        super.onFragmentVisibleChange(isVisible);
+        if (isVisible) {    // 可见
+            mRefreshLayout.autoLoadmore();
+            mRefreshLayout.autoRefresh();
+            orderList.clear();
+            callNetGetCarList();
+        } else {    // 不可见
+            mRefreshLayout.finishRefresh();
+            mRefreshLayout.finishLoadmore();
+        }
+    }
+
+    /**
+     * 请求网络刷新数据
+     */
+    private void callNetGetCarList() {
+        if (orderList == null) {
+            orderList = new ArrayList<>();
+        }
+        for (int i= orderList.size(); i< orderList.size() + 5; i++) {
+            UserOrderOutModel.UserOrderInsideModel.UserOrderModel model = new UserOrderOutModel.UserOrderInsideModel.UserOrderModel();
+            model.setOrderType(2);
+            model.setOrderStatus(2);
+            model.setOrderNo("测试" + i);
+            model.setTotalAmount(100);
+            orderList.add(model);
+        }
     }
 }
