fef48d3ed34fdca11100340c42c182dedb4e20c4
17710622616 <m17710622616@163.com>
2018-03-19 18:57:25 +0800

    完善了帖文详情的界面

--- app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMActivity/Forum/ArticleDetialActivity.java
+++ app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMActivity/Forum/ArticleDetialActivity.java
@@ -105,11 +105,10 @@ public class ArticleDetialActivity extends AppCompatActivity implements View.OnC
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        StatusBarUtil.setColor(this,getResources().getColor(R.color.colorAlpha));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//此FLAG可使状态栏透明，且当前视图在绘制时，从屏幕顶端开始即top = 0开始绘制，这也是实现沉浸效果的基础
-            this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);//可不加
-        }
+        //StatusBarUtil.setColor(this,getResources().getColor(R.color.colorAlpha));
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
         setContentView(R.layout.activity_articel_detial);
         initView();
         setListener();
