f7c3ba8fade50c20a09a90d68a039c19e43da73d
17710622616 <m17710622616@163.com>
2018-07-27 11:07:11 +0800

    论坛导航栏

--- app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMActivity/Forum/ArticleDetialActivity.java
+++ app/src/main/java/com/bs/john_li/bsfslotmachine/BSSMActivity/Forum/ArticleDetialActivity.java
@@ -111,8 +111,10 @@ public class ArticleDetialActivity extends AppCompatActivity implements View.OnC
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //StatusBarUtil.setColor(this,getResources().getColor(R.color.colorAlpha));
-        //getWindow().setStatusBarColor(Color.TRANSPARENT);
+        StatusBarUtil.setColor(this,getResources().getColor(R.color.colorAlpha));
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
         setContentView(R.layout.activity_articel_detial);
